c2e0004f7cf4c8f73b2973128a29486a2b1c249a
Michael Allan <mallan@matchbyte.com>
2013-11-22 21:24:28 +1100

    Specify file to watch from script arguments

--- ch2/watcher.js
+++ ch2/watcher.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
+const filename = process.argv[2];
 
-fs.watch('target.txt', function () {
-    console.log("File 'target.txt' just changed!")
+if (!filename) {
+	console.error('Usage: node watcher <filename> ');
+	process.exit(1);
+}
+
+fs.watch(filename, function () {
+    console.log("File " + filename + " just changed!")
 });
-console.log("Watching for changes to target.txt ...");
+console.log("Watching for changes to " + filename + " ...");
