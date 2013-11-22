0895efcbc28456e2a9a154e0c55722ddf3eaea5c
Michael Allan <mallan@matchbyte.com>
2013-11-22 21:31:41 +1100

    Spawn a child process to watch, with 'use strict' enabling let

--- ch2/watcher.js
+++ ch2/watcher.js
@@ -1,12 +1,17 @@
+'use strict';
+
 const fs = require('fs');
 const filename = process.argv[2];
+const spawn = require('child_process').spawn;
 
 if (!filename) {
-	console.error('Usage: node watcher <filename> ');
-	process.exit(1);
+    console.error('Usage: node watcher <filename> ');
+    process.exit(1);
 }
 
 fs.watch(filename, function () {
-    console.log("File " + filename + " just changed!")
+    let ls = spawn('ls', ['-lh', filename])
+    ls.stdout.pipe(process.stdout);
 });
+
 console.log("Watching for changes to " + filename + " ...");
